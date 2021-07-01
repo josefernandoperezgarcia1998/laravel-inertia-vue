46cdf8d6b37333a4c7b441f82b29a41d52a2f6e0
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-06-30 19:42:47 -0500

    Se importaron los modelos User y Note, al igual que se crea un correo y contraseña para un usuario. No obstante también se crean 30 notas con factory.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Note;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        //Factory para el usuario, cuando se ejecute el factory va a tomar solamente los datos estaticos del correo y contraseña
+        //Y los demás datos como es nombre, etc, los va a tomar de forma fake (aleatoria)
+        User::factory()->create([
+            'email' => 'chitorra_garcia_98@live.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        //Se crearán 30 notas fake
+        Note::factory(30)->create();
     }
 }
