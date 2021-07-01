a0426a7564f4ba3757272127afc833c593014561
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-06-30 20:47:35 -0500

    Se agregó una nueva ruta tipo recurso para las notas

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -24,6 +13,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+//Ruta que está protegida con el componente de autenticación sanctum y se llama dashboard
+Route::get('dashboard', [App\Http\Controllers\PageController::class, 'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('dashboard');
+
+//Ruta tipo recurso para las notas y protegido con el middleware de sanctum
+Route::resource('notes', App\Http\Controllers\NoteController::class)
+    ->middleware('auth:sanctum');
+
