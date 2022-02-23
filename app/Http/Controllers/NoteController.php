404c0a08f1ee5593db76e7caa4fc4ed6b88a2b38
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-23 00:35:00 -0600

    Se agregó el eliminar una nota

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -105,6 +105,7 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        //
+        $note->delete();
+        return redirect()->route('notes.index');
     }
 }
